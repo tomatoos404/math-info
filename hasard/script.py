e1450731c2cd0f93f05926d556efbaf254a4924d
tomatoos404 <thomas.touny991@gmail.com>
2025-11-24 14:17:26 +0100

    exo2

--- hasard/script.py
+++ hasard/script.py
@@ -1,6 +1,13 @@
+# from random import *
+# list = []
+# for i in range(100):
+#     list.append(randint(1, 100))
+# print(list)
+# print(len(list))
+
 from random import *
-list = []
-for i in range(100):
-    list.append(randint(1, 100))
-print(list)
-print(len(list))
+result = []
+for i in range(3):
+    result.append(randint(1, 6))
+print(result)
+print(sum(result))
