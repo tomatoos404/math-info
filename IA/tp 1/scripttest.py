a3d9f72b029f6871d1d2e1d62b692cfaedcd8254
tomatoos404 <thomas.touny991@gmail.com>
2026-04-27 13:59:20 +0200

    fin tp1 ia

--- IA/tp 1/scripttest.py
+++ IA/tp 1/scripttest.py
@@ -1,4 +1,4 @@
-import numpy as np  
+import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
@@ -8,7 +8,10 @@ from sklearn.linear_model import LogisticRegression
 print("✅ Tous les modules sont importés avec succès par mettez votre nom !") 
  
 # Mini test : graphique seaborn 
-data = pd.read_csv("donnee.csv")
+data = pd.DataFrame({ 
+    "vitesse": np.random.normal(80, 15, 100), 
+    "danger": np.random.choice([0, 1], 100) 
+}) 
  
 sns.boxplot(x="danger", y="vitesse", data=data) 
 plt.title("Exemple graphique : vitesse selon danger") 
